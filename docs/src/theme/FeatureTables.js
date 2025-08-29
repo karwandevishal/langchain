f6171f7f0a5a35570f834e539b5f006cede3303b
Vishal Karwande <vishalkarwande@Vishals-MacBook-Pro.local>
2025-08-29 17:20:00 +0530

    added in oci in dropdown

--- docs/src/theme/FeatureTables.js
+++ docs/src/theme/FeatureTables.js
@@ -247,6 +247,17 @@ const FEATURE_TABLES = {
                 "multimodal": true,
                 "local": false,
                 "apiLink": "https://python.langchain.com/api_reference/perplexity/chat_models/langchain_perplexity.chat_models.ChatPerplexity.html"
+            },
+            {
+                "name": "ChatOCI",
+                "package": "langchain-oracle",
+                "link": "oci",
+                "structured_output": true,
+                "tool_calling": false,
+                "json_mode": true,
+                "multimodal": true,
+                "local": false,
+                "apiLink": "https://pypi.org/project/langchain-oci/"
             }
         ],
     },
